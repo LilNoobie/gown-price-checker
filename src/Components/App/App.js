8d7bb48a9bd13567e5ce03dc71d0e2d2810bfc0f
Noobie <jeeee03@gmail.com>
2023-02-16 14:07:37 -0800

    Added Navbar and Routes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,12 +2,21 @@ import React from 'react';
 import './App.css'
 import SearchBar from '../SearchBar/SearchBar';
 import SampleData from '../Data.json';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Navbar from '../Navbar/Navbar';
 import logo from './logo.svg';
 
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route />
+          <Route />
+        </Routes>
+      </Router>
       <header className="App-header">
         <p className="header-text">
           What gown are you looking for?
