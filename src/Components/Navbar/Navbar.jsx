861ff8b6eb8c1cf0cb23adf7b7ae86c0100232e2
Noobie <jeeee03@gmail.com>
2023-02-18 01:38:49 -0800

    Added mailto in Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import './Navbar.css'
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import { fontSize } from "@mui/system";
 
-function Navbar () {
+function Navbar() {
     return (
         <div className="navbar">
             <div className="links">
                 <Link to="/">Home</Link>
                 <Link to="/about">About</Link>
+                <Link to='javascript:void(0)'
+                    onClick={() => window.location = 'mailto:hello@jelyn.co'}><MailOutlineIcon style={{ color: "#cd98b7", fontSize: "22" }} /></Link>
             </div>
         </div>
     );
