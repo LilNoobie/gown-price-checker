d6590f2565da39d8ad9c349ed3c80fad4b8c3583
Noobie <jeeee03@gmail.com>
2023-02-17 17:36:51 -0800

    About page draft

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React from 'react';
+import './About.css'
+
 
 function About() {
   return (
-    <div className="about">
+    <div className="aboutBox">
+      <div className="box">
         <div className="aboutTitle">
-            <h1>About this site</h1>
+            <h1><span id="black">Brides helping brides </span><br></br><span id="pink"> find the one</span></h1>
         </div>
         <div className="aboutText">
-            <p>This is the text for the about</p>
+            <p>Some of the more daunting things in life include buying a car, a house, and surprisingly...a wedding gown.<br></br>
+            <br></br>We want to feel our best in arguably one of the most memorable (and photographed!) events in our life. We pour
+            hours pinning inspos and googling designers. We fall in love with multiple dresses, but we never really know how much
+            they cost until we step into a shop. 
+            
+            <br></br><br></br>It's 2023, let's change that.
+            <br></br> </p>
         </div>
+      </div>
     </div>
   )
 }
--- src/Pages/Dress/DressDetails.jsx
+++ src/Pages/Dress/DressDetails.jsx
@@ -1,14 +1,39 @@
 import React, {useState, useEffect} from 'react';
 import SampleData from '../../Components/Data.json';
 import {Link, useParams} from 'react-router-dom';
+import './DressDetails.css'
+import SampleImage from '../../assets/ines.jpg'
 
-function Dress() {
-  const [dress, setDress] = useState([]);
+
+function DressDetails() {
   const {style} = useParams();
 
-  return (
-    <div className="dressName">Dress Style - {style}</div>
-  )
-}
 
-export default Dress
+  return (
+    <div className="dress">
+
+      <div className="dressImage">
+        <img src={SampleImage} id="sampleImage" />
+      </div>
+
+
+      <div className="dressInfo">
+        <div className="dressName">
+          <h1>{style}</h1>
+            <h2>{SampleData.map(data => {
+              return (
+                <div className="designer">
+                  {data.designer}
+                </div>
+              )
+            })}</h2>
+          </div>
+        <div className="price">Est. Price: $5000</div>
+      </div>
+
+    </div>
+  )
+};
+
+export default DressDetails
+
